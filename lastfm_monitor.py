7c00544c4b274e4c3d11c20881a6b25964083a42
misiektoja <misiektoja-github@rm-rf.ninja>
2026-08-03 01:54:04 +0200

    chore: bump runtime version to 2.6.1

--- lastfm_monitor.py
+++ lastfm_monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: Michal Szymanski <misiektoja-github@rm-rf.ninja>
-v2.6
+v2.6.1
 
 Tool implementing real-time tracking of Last.fm users music activity:
 https://github.com/misiektoja/lastfm_monitor/
@@ -17,7 +17,7 @@ python-dotenv (optional)
 beautifulsoup4 (optional, only for followers/followings tracking)
 """
 
-VERSION = "2.6"
+VERSION = "2.6.1"
 
 # ---------------------------
 # CONFIGURATION SECTION START
