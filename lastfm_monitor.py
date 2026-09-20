bdc5a7131409c5afcee0b724d03da3489b6b1808
misiektoja <misiektoja-github@rm-rf.ninja>
2026-09-21 00:37:53 +0200

    build: declare version 2.8

--- lastfm_monitor.py
+++ lastfm_monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: Michal Szymanski <misiektoja-github@rm-rf.ninja>
-v2.7.1
+v2.8
 
 Tool implementing real-time tracking of Last.fm users music activity:
 https://github.com/misiektoja/lastfm_monitor/
@@ -19,7 +19,7 @@ curl_cffi (optional, only for friends and profile tracking)
 colorama (optional, only for coloured output in the classic Windows Command Prompt)
 """
 
-VERSION = "2.7.1"
+VERSION = "2.8"
 
 # ---------------------------
 # CONFIGURATION SECTION START
